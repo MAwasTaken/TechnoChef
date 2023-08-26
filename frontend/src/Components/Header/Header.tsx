6d2d7d90e6d4ebb55366b828591281969fd069fc
MAwasTaken <ce.mahdiabdollahi@gmail.com>
2023-08-26 20:38:57 +0330

    fix: Header text color

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -22,33 +22,33 @@ const Header: React.FC = () => {
 				{/* navigation */}
 				<ul className="flex items-center justify-between gap-x-4 text-sm text-black md:gap-x-12 md:text-xl">
 					{/* home */}
-					<li className="decoration-red-500 decoration-[2.5px] underline-offset-8 transition-all hover:text-blue-500 hover:underline">
+					<li className="text-slate-300 decoration-red-500 decoration-[2.5px] underline-offset-8 transition-all hover:text-blue-500 hover:underline">
 						<Link to="/">خانه</Link>
 					</li>
 					{/* products */}
-					<li className="decoration-red-500 decoration-[2.5px] underline-offset-8 transition-all hover:text-blue-500 hover:underline">
+					<li className="text-slate-300 decoration-red-500 decoration-[2.5px] underline-offset-8 transition-all hover:text-blue-500 hover:underline">
 						<Link to="/">محصولات</Link>
 					</li>
 					{/* about us */}
-					<li className="decoration-red-500 decoration-[2.5px] underline-offset-8 transition-all hover:text-blue-500 hover:underline">
+					<li className="text-slate-300 decoration-red-500 decoration-[2.5px] underline-offset-8 transition-all hover:text-blue-500 hover:underline">
 						<Link to="/">درباره‌ما</Link>
 					</li>
 				</ul>
 			</div>
 			{/* left section */}
-			<div className="flex items-center text-black justify-between gap-x-3 md:gap-x-6">
+			<div className="flex items-center justify-between gap-x-3 text-black md:gap-x-6">
 				{/* search button */}
 				<GlobalSearch />
 				{/* login / register / panel button */}
-				<button className="relative flex justify-between gap-x-2 rounded-lg border border-orange-500 p-2.5 hover:text-black-100 hover:drop-shadow-[0_0_8px_#f97316] md:gap-x-4 md:border-2 md:p-2 md:px-5">
+				<button className="hover:text-black-100 relative flex justify-between gap-x-2 rounded-lg border border-orange-500 p-2.5 text-slate-300 hover:drop-shadow-[0_0_8px_#f97316] md:gap-x-4 md:border-2 md:p-2 md:px-5">
 					{/* login */}
 					<span className="">ورود</span>
 					{/* divider */}
-					<span className="absolute right-[27px] top-1 md:mx-3 h-3/4 w-px rounded-full bg-red-500 md:right-12 mr-3.5 md:top-1.5 md:w-[2px]">
+					<span className="absolute right-[27px] top-1 mr-3.5 h-3/4 w-px rounded-full bg-red-500 md:right-12 md:top-1.5 md:mx-3 md:w-[2px]">
 						{' '}
 					</span>
 					{/* register */}
-					<span className="md:mr-2 mr-3">ثبت‌نام</span>
+					<span className="mr-3 md:mr-2">ثبت‌نام</span>
 				</button>
 			</div>
 		</header>
