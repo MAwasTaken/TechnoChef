e95f7049bfc72ca4fd2a9cc16c67eb281c4d7432
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-07 03:28:15 +0330

    SignUp and LogIn form upgraded

--- frontend/src/Pages/Signup/Signup.tsx
+++ frontend/src/Pages/Signup/Signup.tsx
@@ -2,6 +2,14 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+// components
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
+// icons
+import { BiLogIn, BiUser } from 'react-icons/bi';
+import { FiLock } from 'react-icons/fi';
+
 // google recaptcha
 import ReCAPTCHA from 'react-google-recaptcha';
 
@@ -18,76 +26,121 @@ const Signup: React.FC = () => {
 
 	// tsx
 	return (
-		<main>
-			{/* logo */}
-			<Link to="/">TechnoShef</Link>
-			{/* title */}
-			<section>
-				<span>ساخت حساب‌کاربری</span>
-				<p>
-					<span>حساب‌کاربری دارید؟</span>
-					<Link to="/login">وارد شوید</Link>
-				</p>
-			</section>
-			{/* form */}
-			<form>
-				{/* first name */}
-				<label htmlFor="firstName">
-					{/* label */}
-					<span>نام</span>
-					{/* input */}
-					<input type="text" id="firstName" />
-				</label>
-				{/* last name */}
-				<label htmlFor="lastName">
-					{/* label */}
-					<span>نام‌خانوادگی</span>
-					{/* input */}
-					<input type="text" id="lastName" />
-				</label>
-				{/* phone */}
-				<label htmlFor="phone">
-					{/* label */}
-					<span>شماره تماس</span>
-					{/* input */}
-					<input type="tel" inputMode="tel" id="phone" />
-				</label>
-				{/* email */}
-				<label htmlFor="email">
-					{/* label */}
-					<span>ایمیل</span>
-					{/* input */}
-					<input type="email" inputMode="email" id="email" />
-				</label>
-				{/* password */}
-				<label htmlFor="password">
-					{/* label */}
-					<span>رمزعبور</span>
-					{/* input */}
-					<input type="password" id="password" />
-				</label>
-				{/* repeat password */}
-				<label htmlFor="repeatPassword">
-					{/* label */}
-					<span>تکرار رمزعبور</span>
-					{/* input */}
-					<input type="password" id="repeatPassword" />
-				</label>
-				{/* google recaptcha */}
-				<section>
-					<ReCAPTCHA sitekey="6LcFwgAoAAAAAEmWVSlMLHjYKEMqcOPu1UnCsUTb" size="normal" />
-				</section>
-				{/* submit button */}
-				<button type="submit">ثبت‌نام</button>
-			</form>
-			{/* rules */}
-			<section>
-				<p>
-					ثبت‌نام شما به معنای پذیرش شرایط و قوانین <Link to="/">تکنوشف</Link> است.
-				</p>
-				<p>رمز عبور باید عبارتی حداقل ۸ کاراکتری شامل حروف بزرگ و کوچک و اعداد لاتین باشد.</p>
-			</section>
-		</main>
+		<div className="flex h-screen flex-col justify-between">
+			<Header />
+			<div className="flex items-center justify-center">
+				<main className="bg-Info/50 flex h-auto w-full flex-col items-center gap-y-2 px-8 py-8 backdrop-blur-[2px] sm:w-3/4 sm:rounded-3xl md:gap-y-4 md:w-3/5 lg:w-1/3 ">
+					{/* logo */}
+					<Link
+					className="font-Lalezar mt-2 text-3xl font-bold tracking-tight text-orange-500 transition-all hover:text-orange-600 md:text-3xl"
+					to="/">TechnoShef</Link>
+					{/* title */}
+					<section className="flex w-full items-center justify-between">
+					<h2 className="font-Lalezar flex select-none items-center gap-x-[2px] text-2xl md:gap-x-1 md:text-3xl">
+							<BiLogIn className="text-red-500" />
+							<span>ساخت حساب‌کاربری</span>
+						</h2>
+					</section>
+					{/* form */}
+					<form className="flex flex-col gap-y-2 md:gap-y-4">
+						{/* first name */}
+						<label className="flex items-center justify-center" htmlFor="firstName">
+							{/* label */}
+							<span>نام</span>
+							{/* input */}
+							<input className="border-2 border-gray-300 text-base focus:outline-none focus:border-DarkYellow rounded-lg py-2 px-4 w-10/12 text-right" 
+								type="text"
+								placeholder="نام"
+								id="firstName"
+								dir="ltr" />
+						</label>
+						{/* last name */}
+						<label htmlFor="lastName">
+							{/* label */}
+							<span>نام‌خانوادگی</span>
+							{/* input */}
+							<input className="border-2 border-gray-300 text-base focus:outline-none focus:border-DarkYellow rounded-lg py-2 px-4 w-10/12 text-right" 
+								type="text"
+								placeholder="نام خانوادگی"
+								id="lastName"
+								dir="ltr" />						</label>
+						{/* phone */}
+						<label htmlFor="phone">
+							{/* label */}
+							<span>شماره تماس</span>
+							{/* input */}
+							<input className="border-2 border-gray-300 text-base focus:outline-none focus:border-DarkYellow rounded-lg py-2 px-4 w-10/12 text-right" 
+								type="tel"
+								placeholder="شماره تماس"
+								id="phone"
+								dir="ltr" />
+						</label>
+						{/* email */}
+						<label htmlFor="email">
+							{/* label */}
+							<span>ایمیل</span>
+							{/* input */}
+							<input className="border-2 border-gray-300 text-base focus:outline-none focus:border-DarkYellow rounded-lg py-2 px-4 w-10/12 text-right" 
+								type="email"
+								placeholder="ایمیل"
+								id="email"
+								dir="ltr" 
+								inputMode="email"/>
+						</label>
+						{/* password */}
+						<label htmlFor="password">
+							{/* label */}
+							<span>رمزعبور</span>
+							{/* input */}
+							<input className="border-2 border-gray-300 text-base focus:outline-none focus:border-DarkYellow rounded-lg py-2 px-4 w-10/12 text-right" 
+								type="password"
+								placeholder="رمز عبور"
+								id="password"
+								dir="ltr" />						</label>
+						{/* repeat password */}
+						<label htmlFor="repeatPassword">
+							{/* label */}
+							<span>تکرار رمزعبور</span>
+							{/* input */}
+							<input className="border-2 border-gray-300 text-base focus:outline-none focus:border-DarkYellow rounded-lg py-2 px-4 w-10/12 text-right" 
+								type="password"
+								placeholder="تکرار رمز عبور"
+								id="repeatPassword"
+								dir="ltr" />						</label>
+						{/* google recaptcha */}
+						<section>
+							<ReCAPTCHA sitekey="6LcFwgAoAAAAAEmWVSlMLHjYKEMqcOPu1UnCsUTb" size="normal" />
+						</section>
+						{/* submit button */}
+						<button
+							className="font-Lalezar from-LightYellow to-DarkYellow mx-auto mt-2 flex w-24 items-center justify-center rounded-lg bg-gradient-to-r p-1.5 text-base shadow-md transition-all hover:bg-gradient-to-t md:mt-4 md:w-[150px] md:p-2 md:text-lg"
+							type="submit"
+						>
+							ورود
+						</button>
+					</form>
+					{/* login */}
+					<p className="flex mt-3 w-full items-center justify-center gap-x-[2px] text-xs md:gap-x-1 md:text-sm">
+							<span className="md:inline-block md:text-sm">حساب‌کاربری دارید؟</span>
+							<Link className="text-blue-600 hover:text-blue-700" to="/login">
+								وارد شوید.
+							</Link>
+						</p>
+					{/* rules */}
+					<ul className="text-Dark/75 list-disc text-xs self-start px-8 py-4">
+						<li>
+						ثبت‌نام شما به معنای پذیرش شرایط و قوانین{' '}
+							<Link className="text-orange-500" to="/">
+								تکنوشف
+							</Link>{' '}
+							است.
+						</li>
+						<li>رمز عبور باید عبارتی حداقل ۸ کاراکتری شامل حروف بزرگ و کوچک و اعداد لاتین باشد.</li>
+					</ul>
+				</main>
+			</div>
+			<Footer />
+			</div>
 	);
 };
 
