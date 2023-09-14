e1e84427548753cbb230a30d91e0628ef8d58388
hossein jahandideh <jahandidehh@hotmail.com>
2023-09-14 17:48:39 +0330

    🔨| fix the phoneNumber regex in the validator.

--- backend/validator/userValidation.js
+++ backend/validator/userValidation.js
@@ -8,7 +8,7 @@ const model = joi.object({
 	password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).min(8),
 	email: joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'ir', 'net', 'org'] } }),
 	isAdmin: joi.boolean(),
-	phoneNumber: joi.string().pattern(new RegExp('^09[0|1|2|3][0-9]{8}$')),
+	phoneNumber: joi.string().pattern(new RegExp('^(\\+98|0)?9\\d{9}$')),
 	postalCode: joi.string().pattern(new RegExp('\\b(?!(\\d)\\1{3})[13-9]{4}[1346-9][013-9]{5}\\b')),
 	nationalCode: joi.string().pattern(new RegExp('^[0-9]{10}$')),
 	address: joi.string()
