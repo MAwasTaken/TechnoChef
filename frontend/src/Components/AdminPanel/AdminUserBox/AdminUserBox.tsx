41351ab12a865b55fcd5a847fbb740f71d768ccb
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-24 10:43:09 +0330

    Create:AdminUser page created and updated

---
+++ frontend/src/Components/AdminPanel/AdminUserBox/AdminUserBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AdminUserBox = () => {
+    return (
+        <div className='flex justify-center items-center'>
+            <div className="flex flex-col w-full h-10 bg-gray-500">
+
+            </div>
+        </div >
+    )
+}
+
+export default AdminUserBox
--- frontend/src/Components/AdminPanel/Users/Users.tsx
+++ frontend/src/Components/AdminPanel/Users/Users.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BiPlus, BiUser } from 'react-icons/bi';
+import AdminUserBox from './../AdminUserBox/AdminUserBox';
 
 const Users = () => {
   return (
-    <div className='flex justify-center items-center'>Users</div>
+    <div className='flex flex-col gap-y-5 p-5 justify-center items-center'>
+        <label htmlFor="" className='flex flex-row w-full gap-x-2 text-3xl'><BiUser className='text-red-500'></BiUser><span>کاربران فعال شده</span></label>
+        {/* divider */}
+        <div className="border-t border-gray-700 w-full"></div>
+        {/* Users */}
+        <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-row w-full bg-gray-500 "></div>
+            <AdminUserBox />
+        </div>
+    </div>
   )
 }
 
