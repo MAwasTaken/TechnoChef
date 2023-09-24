1d0331253b35d3b31e3a0bb26c30cf185297edfe
hossein jahandideh <jahandidehh@hotmail.com>
2023-09-24 14:25:29 +0330

    🔨| add new origin url to cors options.

--- backend/server/corsConfig.js
+++ backend/server/corsConfig.js
@@ -1,4 +1,8 @@
-const allowedOrigins = ['https://www.technoshef.com', 'http://localhost:5173'];
+const allowedOrigins = [
+	'https://www.technoshef.com',
+	'http://localhost:5173',
+	'http://127.0.0.1:5173'
+];
 
 const corsOptions = {
 	origin: (origin, callback) => {
