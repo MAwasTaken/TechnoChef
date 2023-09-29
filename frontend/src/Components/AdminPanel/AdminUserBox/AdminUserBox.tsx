f4ce3277aa4d7710fa02e5e795f43c6ed2bd479e
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-29 14:24:57 +0330

    fix:Userbox upgrading , not finished

--- frontend/src/Components/AdminPanel/AdminUserBox/AdminUserBox.tsx
+++ frontend/src/Components/AdminPanel/AdminUserBox/AdminUserBox.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+// import { Accordion, AccordionSummary, AccordionDetails, Typography } from "@mui/material";
+// import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-const AdminUserBox = () => {
-    return (
-        <div className='flex justify-center items-center'>
-            <div className="flex flex-col w-full h-10 bg-gray-500">
 
-            </div>
-        </div >
-    )
+interface UserItem {
+    title: string;
+    content: string;
 }
+
+interface UserProps {
+    items: UserItem[];
+}
+
+const AdminUserBox: React.FC<UserProps> = ({ items }) => {
+    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+    const toggleAccordion = (index: number) => {
+        setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+    };
+
+    return (
+        <div>
+            {/* {items.map((item, index) => (
+                <Accordion key={index}>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>{item.title}</Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Typography>{item.content}</Typography>
+                    </AccordionDetails>
+                </Accordion>
+            ))} */}
+        </div>
+    );
+};
 
 export default AdminUserBox
