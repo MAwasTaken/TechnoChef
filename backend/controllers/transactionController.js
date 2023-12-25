a2c8c54649bee23da266e5a53e3aa0309bda8fdc
milad-taghizadehm <miladtaghizadeh1382@outlook.com>
2023-12-25 21:55:05 +0330

    FIX: transaction callback url.

--- backend/controllers/transactionController.js
+++ backend/controllers/transactionController.js
@@ -23,7 +23,7 @@ const transactionGatewayController = async (req, res, next) => {
 				email: user?.email || 'example@domain.com',
 				mobile: user?.phoneNumber
 			},
-			callback_url: 'http://localhost:5173/orders'
+			callback_url: 'http://localhost:5173/panel/orders'
 		};
 
 		const transactionRequestResult = await axios
