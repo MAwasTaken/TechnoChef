72e29e32180358d63c795512a5adaad8385afdfc
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2024-03-06 01:45:35 +0330

    FIX : mongoose configuration fixed

--- db/config/mongooseConfig.js
+++ db/config/mongooseConfig.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 // connect to mongoose
 mongoose
-.connect(process.env.DB_URL)
+.connect(process.env.DB_URL+"/"+process.env.DB_NAME)
 .then(() => {
     console.log(`MongoDB started and (${process.env.DB_NAME}) is now available`);
 })
